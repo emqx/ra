emqx/ra

commit b0df49d4ad350f4f08358b401094c88a90933833

ra_server: revert to initial cluster when `prev_cluster` is unset

src/ra_server.erl
@@ -3106,7 +3106,8 @@ append_log_leader(Cmd, #{log := Log0, current_term := Term} = State, Effects) ->
     end.
 
 pre_append_log_follower({Idx, Term, Cmd} = Entry,
-                        State = #{cluster_index_term := {Idx, CITTerm}})
+                        State = #{cluster_index_term := {Idx, CITTerm},
+                                  id := Id})
   when Term /= CITTerm ->
     % the index for the cluster config entry has a different term, i.e.
     % it has been overwritten by a new leader. Unless it is another cluster
@@ -3120,7 +3121,14 @@ pre_append_log_follower({Idx, Term, Cmd} = Entry,
                    cluster_index_term => {Idx, Term}};
         _ ->
             % revert back to previous cluster
-            {PrevIdx, PrevTerm, PrevCluster} = maps:get(previous_cluster, State),
+            {PrevIdx, PrevTerm, PrevCluster} =
+                case State of
+                    #{previous_cluster := Prev} ->
+                        Prev;
+                    #{} ->
+                        InitialMembers = state_query(initial_members, State),
+                        {0, 0, make_cluster(Id, InitialMembers)}
+                end,
             ?DEBUG("~ts: ~ts: follower reverting cluster change to ~w",
                    [log_id(State), ?FUNCTION_NAME, maps:keys(PrevCluster)]),
             State1 = State#{cluster => PrevCluster,

commit 965be8b30ea8ddbce66901447adcbdf86ba0c7f7

ra_server: Fix pattern matching id in `pre_append_log_follower/2`
This commit fixes the logic and the corresponding testcase
`leader_appends_cluster_change_then_steps_before_applying_it`,
which were broken by b0df49d4.

src/ra_server.erl
@@ -3107,7 +3107,7 @@ append_log_leader(Cmd, #{log := Log0, current_term := Term} = State, Effects) ->
 
 pre_append_log_follower({Idx, Term, Cmd} = Entry,
                         State = #{cluster_index_term := {Idx, CITTerm},
-                                  id := Id})
+                                  cfg := #cfg{id = Id}})
   when Term /= CITTerm ->
     % the index for the cluster config entry has a different term, i.e.
     % it has been overwritten by a new leader. Unless it is another cluster